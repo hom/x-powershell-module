4fd9848e3c49a115ce09b889cc2defb7cdb8677c
mengj <813935532@qq.com>
2022-05-03 21:53:00 +0800

    feat: add X-New-Problem

---
+++ src/X-New-Problem/X-New-Problem.psm1
@@ -0,0 +1,15 @@
+function X-New-Problem ($name) {
+  $directory = Join-Path -Path D:\Ruxf\leetcode\src\ -ChildPath $name
+  if ((Test-Path $directory) -eq "True") {
+    return Write-Warning(-Join('The directory ', $directory, ' is already exists.'))
+  }
+  New-Item -Type Directory -Path $directory
+  New-Item $directory\index.js
+  New-Item $directory\problem.md
+  New-Item $directory\test.js
+}
+
+New-Alias xnp X-New-Problem
+
+Export-ModuleMember -Function X-New-Problem
+Export-ModuleMember -Alias xnp
