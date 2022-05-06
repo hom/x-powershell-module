763a0746887a5e38b44fc725f3be18b15d3d9e4b
mengj <813935532@qq.com>
2022-05-06 21:43:37 +0800

    fix: rename prompt to Prompt

--- src/X-Set-Proxy-Prompt/X-Set-Proxy-Prompt.psm1
+++ src/X-Set-Proxy-Prompt/X-Set-Proxy-Prompt.psm1
@@ -2,7 +2,7 @@ function X-Set-Proxy-Prompt () {
   "PROXY $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
 }
 
-New-Alias -Name prompt -Value X-Set-Proxy -Scope Global
+New-Alias -Name Prompt -Value X-Set-Proxy-Prompt -Scope Global
 
 Export-ModuleMember -Function X-Set-Proxy-Prompt
-Export-ModuleMember -Alias prompt
+Export-ModuleMember -Alias Prompt
