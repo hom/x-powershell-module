9fac59aec7ec0431d7bd90624c6eedad23655e7f
mengj <813935532@qq.com>
2022-05-06 15:03:07 +0800

    feat: add proxy prefix

--- src/X-Set-Proxy/X-Set-Proxy.psm1
+++ src/X-Set-Proxy/X-Set-Proxy.psm1
@@ -3,6 +3,10 @@ function X-Set-Proxy () {
   Set-Item Env:https_proxy -Value 'http://127.0.0.1:7890'
 }
 
+function Prompt () {
+  "`e[;32mProxy`e[m $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) ";
+}
+
 New-Alias -Name xsp -Value X-Set-Proxy -Scope Global
 
 Export-ModuleMember -Function X-Set-Proxy
