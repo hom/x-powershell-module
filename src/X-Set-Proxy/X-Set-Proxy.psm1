4154fe3a65af105c274f946b491613731f2f2787
mengj <813935532@qq.com>
2022-05-06 18:20:09 +0800

    feat: add remove proxy

--- src/X-Set-Proxy/X-Set-Proxy.psm1
+++ src/X-Set-Proxy/X-Set-Proxy.psm1
@@ -1,10 +1,18 @@
 function X-Set-Proxy () {
-  Set-Item Env:http_proxy -Value 'http://127.0.0.1:7890'
-  Set-Item Env:https_proxy -Value 'http://127.0.0.1:7890'
-  . C:\Windows\System32\WindowsPowerShell\v1.0\Modules\X-Set-Proxy\Prompt.ps1
+  Set-Item Env:HTTP_PROXY -Value 'http://127.0.0.1:7890'
+  Set-Item Env:HTTPS_PROXY -Value 'http://127.0.0.1:7890'
+  Write-Host "Set proxy channel"
+}
+function X-Remove-Proxy () {
+  Remove-Item Env:HTTP_PROXY
+  Remove-Item Env:HTTPS_PROXY
+  Write-Host "Remove proxy channel"
 }
 
 New-Alias -Name xsp -Value X-Set-Proxy -Scope Global
+New-Alias -Name xrp -Value X-Remove-Proxy -Scope Global
 
 Export-ModuleMember -Function X-Set-Proxy
+Export-ModuleMember -Function X-Remove-Proxy
 Export-ModuleMember -Alias xsp
+Export-ModuleMember -Alias xrp
