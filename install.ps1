dbb364616da6d4bce9ab909fd4c2a09025e56497
mengj <813935532@qq.com>
2022-09-25 21:28:51 +0800

    feat: add run as adminstrator alert

--- install.ps1
+++ install.ps1
@@ -2,8 +2,18 @@
 $location = Get-Location
 $modules = Get-ChildItem -Path "$location\src\" -Directory
 
-foreach ($module in $modules) {
-    Write-Host $module
-    # 添加软链接到用户模块目录
-    New-Item -ItemType SymbolicLink -Path "$HOME\Documents\WindowsPowerShell\Modules\$module" -Target "$location\src\$module" -Force
+function Test-Adminstrator {
+    [bool] (
+        (whoami /groups) -Match "S-1-16-12288"
+    )
+    
+}
+if (Test-Adminstrator) {
+    foreach ($module in $modules) {
+        Write-Host $module
+        # 添加软链接到用户模块目录
+        New-Item -ItemType SymbolicLink -Path "$HOME\Documents\WindowsPowerShell\Modules\$module" -Target "$location\src\$module" -Force
+    }
+} else {
+    Write-Error "This script neetd to run as Adminstrator"
 }
